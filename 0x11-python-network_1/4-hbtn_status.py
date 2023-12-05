df4a03d2d4dafd97b22e03c6749862d071cd3d81
Richard Miruka Onsare <richardonsare15@gmail.com>
2023-12-06 01:19:01 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+fetching url using request package
+"""
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    text = r.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(text)))
+    print('\t- content: {}'.format(text))
