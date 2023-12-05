be5032c7d7f21c519a28014db0ca09a9486d2b23
Richard Miruka Onsare <richardonsare15@gmail.com>
2023-12-06 01:23:51 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+# sends a JSON request to url passed
+curl -s -X POST -d "@$2" -H "Content-Type: application/json" "$1"
